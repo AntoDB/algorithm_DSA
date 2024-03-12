05ae74052a90fa9adca341833cc4778b5fe2fb02
Antonin <antonindebreuck@gmail.com>
2024-03-12 12:30:01 +0100

    Add file, work and fast !

---
+++ !9. Nearly sorted - v4.py
@@ -0,0 +1,42 @@
+#User function Template for python3
+
+class Solution:
+    
+    #Function to return the sorted array.
+    def nearlySorted(self,a,n,k):
+        # code here
+        sorted_list = a[::]
+        #print(sorted_list)
+        sorted_list.sort()
+        #print(sorted_list)
+        return sorted_list
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+import heapq
+from collections import  defaultdict
+
+# Contributed by : Nagendra Jha
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases) :
+        n,k = map(int,input().strip().split())
+        a = list(map(int,input().strip().split()))
+        ob=Solution()
+        print(*ob.nearlySorted(a,n,k))
+
+# } Driver Code Ends
+
+
+
+# Tested inputs :
+'''
+1
+6 4
+1 3 5 6 4 2
+'''
